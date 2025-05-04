6d8e45a38176514602e82bbb5280a429d48c8010
MSTS-S <seri.mmm.0x0@gmail.com>
2025-05-04 13:19:07 +0900

    change profile data & organize component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate, useLocation } from 'react-router-dom';
 import './App.css';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
@@ -12,40 +12,35 @@ import Section_Qualification from './components/Section_Qualification/Qualificat
 import Section_Unity from './components/Section_Unity/Unity';
 import Section_ReactFunctions from './components/Section_ReactFunctions/ReactFunction';
 import Section_LinkContact from './components/Section_LinkContact/LinkContact';
-import Section_Copyright from './components/Section_Copyright/Copyright';
-
 
 /* import functions */
 import QRCodeGenerator from './functions/QRCodeGenerator/QRCodeGenerator';
 import LoginForm from './functions/LoginForm/LoginForm';
 
 /* import img */
-import HeaderImage from './components/img/Header.png';
+import HEADER_IMAGE from './components/img/Header.png';
 
 /* import MUI ICON */
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
-import SchoolIcon from '@mui/icons-material/School';
-import TerminalIcon from '@mui/icons-material/Terminal';
-import CreditCardIcon from '@mui/icons-material/CreditCard';
-import AppsIcon from '@mui/icons-material/Apps';
-import FunctionsIcon from '@mui/icons-material/Functions';
-import LinkIcon from '@mui/icons-material/Link';
-import XIcon from '@mui/icons-material/X';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import MailIcon from '@mui/icons-material/Mail';
+import PROFILE_ICON from '@mui/icons-material/AccountBox';
+import CAREER_HISTORY_ICON from '@mui/icons-material/HistoryEdu';
+import RESEARCH_ICON from '@mui/icons-material/School';
+import PROGRAMING_ICON from '@mui/icons-material/Terminal';
+import QUALIFICATION_ICON from '@mui/icons-material/CreditCard';
+import UNITY_APPLICATION_ICON from '@mui/icons-material/Apps';
+import REACT_FUNCTIONS_ICON from '@mui/icons-material/Functions';
+import LINK_ICON from '@mui/icons-material/Link';
+import X_ICON from '@mui/icons-material/X';
+import INSTAGRAM_ICON from '@mui/icons-material/Instagram';
+import MAIL_ICON from '@mui/icons-material/Mail';
 
 /* https://mui.com/material-ui/react-drawer/#persistent-drawer */
 /* MUI Drawer */
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -56,71 +51,192 @@ const SectionComponentData = [
     id: 'profile',
     title: 'Profile',
     component: <Section_Profile />,
-    icon: <AccountBoxIcon />,
+    icon: <PROFILE_ICON />,
   },
   {
     id: 'history',
     title: 'Career History',
     component: <Section_History />,
-    icon: <HistoryEduIcon />,
+    icon: <CAREER_HISTORY_ICON />,
   },
   {
     id: 'academicresearch',
     title: 'Academic Research',
     component: <Section_AcademicResearch />,
-    icon: <SchoolIcon />,
+    icon: <RESEARCH_ICON />,
   },
   {
     id: 'programing',
     title: 'Programing',
     component: <Section_Programing />,
-    icon: <TerminalIcon />,
+    icon: <PROGRAMING_ICON />,
   },
   {
     id: 'qualification',
-    title: 'Qulifications',
+    title: 'Qualifications',
     component: <Section_Qualification />,
-    icon: <CreditCardIcon />,
+    icon: <QUALIFICATION_ICON />,
   },
   {
     id: 'unity',
     title: 'Unity App',
     component: <Section_Unity />,
-    icon: <AppsIcon />,
+    icon: <UNITY_APPLICATION_ICON />,
   },
   {
     id: 'functions',
     title: 'React Functions',
     component: <Section_ReactFunctions />,
-    icon: <FunctionsIcon />,
+    icon: <REACT_FUNCTIONS_ICON />,
   },
   {
     id: 'linkcontact',
     title: 'Link / Contact',
     component: <Section_LinkContact />,
-    icon: <LinkIcon />,
+    icon: <LINK_ICON />,
   },
 ];
 
 const ContactData = [
   {
     name: 'X (Twitter)',
-    icon: <XIcon />,
+    icon: <X_ICON />,
     link: 'https://msts-hp.com/'
   },
   {
     name: 'Instagram',
-    icon: <InstagramIcon />,
+    icon: <INSTAGRAM_ICON />,
     link: 'https://www.instagram.com/m_seri_m/'
   },
   {
     name: 'E-Mail',
-    icon: <MailIcon />,
+    icon: <MAIL_ICON />,
     link: 'mailto:serizawa-masatoshi@ed.tmu.ac.jp'
   },
 ];
 
-function AppContents() {
+/* Constant Number regarding Drawer */
+const DRAWER_POSITION = "right"; // can change to 'left', 'top', 'bottom'
+const DRAWER_WIDTH = 300;
+
+function Header() {
+  const [_isMenuOpen, setIsMenuOpen] = useState(false); // メニューのオープン状態を管理
+
+  const navigate = useNavigate();
+
+  const handleTitleClick = () => {
+    navigate('/');
+  };
+
+  const handleLinkClick = (url) => {
+    window.open(url, '_blank');
+  };
+
+  /* Switching the state of drawer (open : close) */
+  const toggleDrawer = () => (event) => {
+    /* Assuming an external keyboard on the ipad, do not close the drawer with tab or shift key. */
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    /* update bool of _isMenuOpen */
+    setIsMenuOpen(!_isMenuOpen);
+  };
+
+  /* Tab list display in hamburger menu */
+  const renderList = () => (
+    <Box
+      sx={{ width: DRAWER_POSITION === 'top' || DRAWER_POSITION === 'bottom' ? 'auto' : DRAWER_WIDTH }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <br />
+      <List>
+        <br />
+        <div className='header__categoryName'>
+          CONTENTS
+        </div>
+        <Divider />
+        {SectionComponentData.map((tab) => (
+          <ListItem key={tab.id} disablePadding>
+            <ListItemButton component="a" href={`#${tab.id.toLowerCase()}`}>
+              <ListItemIcon >
+                {tab.icon}
+              </ListItemIcon>
+              <ListItemText primary={tab.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <br />
+      <div className='header__categoryName'>
+        SNS / CONTACT
+      </div>
+      <Divider />
+      <List>
+        {ContactData.map(({ name, icon, link }, index) => (
+          <ListItem key={name} disablePadding>
+            <ListItemButton onClick={() => handleLinkClick(link)}>
+              <ListItemIcon>
+                {icon}
+              </ListItemIcon>
+              <ListItemText primary={name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div className='header__container'>
+      <a
+        className='header__webSiteTitle'
+        onClick={() => handleTitleClick()}
+        href={'/'}
+      >
+        <div className='header__logoTMU'>
+          <img src={HEADER_IMAGE} alt='header image' />
+        </div>
+      </a>
+
+      {/* Display hamburger menu */}
+      <div className='header__hamburgerMenu'>
+        <div className={`header__hamburgerMenuIcon ${_isMenuOpen ? 'active' : ''}`}>
+          <React.Fragment>
+            {/* Drawer Trigger */}
+            <Button onClick={toggleDrawer()}><HamburgerMenu /></Button>
+            <Drawer
+              className='header__customDrawer'
+              anchor={DRAWER_POSITION}
+              open={_isMenuOpen}
+              onClose={toggleDrawer()}
+              ModalProps={{
+                keepMounted: true,
+                disableScrollLock: false,
+              }}
+            >
+              {renderList()}
+            </Drawer>
+          </React.Fragment>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function HamburgerMenu() {
+  return (
+    <div className='hamburgerMenu__Line'>
+      <div className='line'></div>
+      <div className='line'></div>
+      <div className='line'></div>
+    </div>
+  );
+}
+
+
+function Body() {
   const theme = useTheme();
   const isMobile = useMediaQuery('(max-width:768px)');
   const drawerWidth = isMobile ? '100vw' : 300;
@@ -130,177 +246,54 @@ function AppContents() {
 
   const [open, setOpen] = React.useState(false);
 
-  // Drawerのスクロール制御
-  useEffect(() => {
-    if (open && isMobile) {
-      // モバイルの場合（画面幅768px以下）は全体のスクロールを無効化
-      document.body.style.overflow = 'hidden';
-    } else {
-      // それ以外の場合はスクロールを有効に戻す
-      document.body.style.overflow = 'auto';
-    }
-
-    // クリーンアップ関数で、Drawerが閉じられたときやコンポーネントがアンマウントされたときにスクロールを元に戻す
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, [open, isMobile]);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const handleLinkClick = (url) => {
-    window.open(url, '_blank');
-  };
-
-  const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme }) => ({
-      flexGrow: 1,
-      padding: theme.spacing(0),
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      marginRight: -drawerWidth,
-      position: 'relative',
-      variants: [
-        {
-          props: ({ open }) => open,
-          style: {
-            transition: theme.transitions.create('margin', {
-              easing: theme.transitions.easing.easeOut,
-              duration: theme.transitions.duration.enteringScreen,
-            }),
-            marginRight: 0,
-          },
-        },
-      ],
-    }),
-  );
-
-  const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    justifyContent: 'flex-start',
-  }));
-
   return (
-    <Box sx={{ display: 'flex' }} className="App">
-      <CssBaseline />
-      <Main open={open} sx={{ marginRight: open ? 0 : `-${drawerWidth}` }}>
-        <DrawerHeader />
-        <div className="App">
+    <div className="body__container">
+      {SectionComponentData.map(({ id, component }, index) => (
+        <div
+          id={id.toLowerCase()}
+          key={index}
+          className={
+            index === 0 ? 'backgroundColor-profile'
+              : index % 2 === 0 ? 'backgroundColor-even'
+                : 'backgroundColor-odd'
+          }
+          style={{ padding: '5%' }}
+        >
+          {component}
+        </div>
+      ))}
+    </div>);
+}
 
-          {/* HEADER */}
-          <header className="App-header">
-            <div className="header-left">
-              <div className="App-logo">
-                <img src={HeaderImage} alt="Logo" />
-              </div>
-            </div>
-            <div className="header-right">
-              <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawerOpen} sx={[open && { display: 'none' }]}>
-                <MenuIcon />
-              </IconButton>
-            </div>
-          </header>
-
-          {/* BODY */}
-          <div className="App-body">
-            {SectionComponentData.map(({ id, component }, index) => (
-              <div
-                id={id.toLowerCase()}
-                key={index}
-                className={
-                  index === 0 ? 'backgroundColor-profile'
-                    : index % 2 === 0 ? 'backgroundColor-even'
-                      : 'backgroundColor-odd'
-                }
-                style={{ padding: '5%' }}
-              >
-                {component}
-              </div>
-            ))}
-          </div>
-
-          {/* FOOTER */}
-          <div className="App-footer">
-            <div className='backgroundColor-copyright' style={{ padding: '5%' }}>
-              <Section_Copyright />
-            </div>
-          </div>
-        </div >
-      </Main>
-
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-            position: 'fixed',
-          },
-        }}
-        variant="persistent"
-        anchor="right"
-        open={open}
-        className='drawer'
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronRightIcon />
-          </IconButton>
-        </DrawerHeader>
-        <br />
-        CONTENTS
-        <Divider />
-        <List>
-          {SectionComponentData.map(({ id, title, icon }, index) => (
-            <ListItem key={id} disablePadding>
-              <ListItemButton component="a" href={`#${id.toLowerCase()}`} onClick={isMobile ? handleDrawerClose : undefined}>
-                <ListItemIcon >
-                  {icon}
-                </ListItemIcon>
-                <ListItemText primary={title} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <br />
-        SNS / CONTACT
-        <Divider />
-        <List>
-          {ContactData.map(({ name, icon, link }, index) => (
-            <ListItem key={name} disablePadding>
-              <ListItemButton onClick={() => handleLinkClick(link)}>
-                <ListItemIcon>
-                  {icon}
-                </ListItemIcon>
-                <ListItemText primary={name} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-    </Box>
+function Footer() {
+  return (
+    <div className='footer__container'>
+      <div className='footer__text'>
+        <div> Copyright © 2025 Masatoshi SERIZAWA </div>
+        <div> &nbsp; All rights reserved. &nbsp; </div>
+      </div>
+    </div>
   );
 }
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<AppContents />} />
-        <Route path="/qrcodegenerator" element={<QRCodeGenerator />} />
-        <Route path="/loginform" element={<LoginForm />} />
-      </Routes>
+      <div className='App__container'>
+        <div className='App__header'>
+          <Header />
+        </div>
+        <div className='App__body'>
+          <Routes>
+            <Route path="/" element={<Body />} />
+            <Route path="/qrcodegenerator" element={<QRCodeGenerator />} />
+            <Route path="/loginform" element={<LoginForm />} />
+          </Routes>
+        </div>
+        <div className='App__footer'>
+          <Footer />
+        </div>
+      </div>
     </Router>
   );
 }
